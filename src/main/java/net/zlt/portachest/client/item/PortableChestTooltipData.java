94c3e09b1799181236a9bf1548bf5e7aec4ffacd
ZLT <manult002@gmail.com>
2025-01-07 14:46:31 -0300

    add portable chest tooltip data environment annotation

--- src/main/java/net/zlt/portachest/client/item/PortableChestTooltipData.java
+++ src/main/java/net/zlt/portachest/client/item/PortableChestTooltipData.java
@@ -1,9 +1,12 @@
 package net.zlt.portachest.client.item;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.item.TooltipData;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.collection.DefaultedList;
 
+@Environment(EnvType.CLIENT)
 public class PortableChestTooltipData implements TooltipData {
     private final DefaultedList<ItemStack> stacks;
 
