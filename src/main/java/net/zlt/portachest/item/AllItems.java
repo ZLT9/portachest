ad8260b75240a52235390a3828fa88670277bfed
ZLT <manult002@gmail.com>
2025-01-07 16:10:25 -0300

    add portable chest extending method overrides

--- src/main/java/net/zlt/portachest/item/AllItems.java
+++ src/main/java/net/zlt/portachest/item/AllItems.java
@@ -1,8 +1,10 @@
 package net.zlt.portachest.item;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.xanthian.variantvanillablocks.block.Chests;
 import net.zlt.portachest.Portachest;
 import net.zlt.portachest.compat.Mods;
 
@@ -15,25 +17,125 @@ public final class AllItems {
     public static final Item PORTABLE_CHEST = register("portable_chest", new PortableChestItem(Portachest.asId("textures/entity/portable_chest.png"), new Item.Settings().maxCount(1)));
     public static final Item PORTABLE_LARGE_CHEST = register("portable_large_chest", new PortableLargeChestItem(Portachest.asId("textures/entity/portable_large_chest.png"), new Item.Settings().maxCount(1)));
 
-    public static final Item PORTABLE_SPRUCE_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_spruce_chest", () -> new PortableChestItem(Portachest.asId("textures/entity/portable_spruce_chest.png"), new Item.Settings().maxCount(1)));
+    public static final Item PORTABLE_SPRUCE_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_spruce_chest", () -> new PortableChestItem(Portachest.asId("textures/entity/portable_spruce_chest.png"), new Item.Settings().maxCount(1)) {
+        @Override
+        public boolean canBeExtendedWith(ItemStack stack) {
+            return !stack.isEmpty() && stack.isOf(Chests.SPRUCE_CHEST.asItem());
+        }
+
+        @Override
+        public ItemStack getLarge() {
+            return PORTABLE_LARGE_SPRUCE_CHEST.getDefaultStack();
+        }
+    });
     public static final Item PORTABLE_LARGE_SPRUCE_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_large_spruce_chest", () -> new PortableLargeChestItem(Portachest.asId("textures/entity/portable_large_spruce_chest.png"), new Item.Settings().maxCount(1)));
-    public static final Item PORTABLE_BIRCH_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_birch_chest", () -> new PortableChestItem(Portachest.asId("textures/entity/portable_birch_chest.png"), new Item.Settings().maxCount(1)));
+    public static final Item PORTABLE_BIRCH_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_birch_chest", () -> new PortableChestItem(Portachest.asId("textures/entity/portable_birch_chest.png"), new Item.Settings().maxCount(1)) {
+        @Override
+        public boolean canBeExtendedWith(ItemStack stack) {
+            return !stack.isEmpty() && stack.isOf(Chests.BIRCH_CHEST.asItem());
+        }
+
+        @Override
+        public ItemStack getLarge() {
+            return PORTABLE_LARGE_BIRCH_CHEST.getDefaultStack();
+        }
+    });
     public static final Item PORTABLE_LARGE_BIRCH_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_large_birch_chest", () -> new PortableLargeChestItem(Portachest.asId("textures/entity/portable_large_birch_chest.png"), new Item.Settings().maxCount(1)));
-    public static final Item PORTABLE_JUNGLE_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_jungle_chest", () -> new PortableChestItem(Portachest.asId("textures/entity/portable_jungle_chest.png"), new Item.Settings().maxCount(1)));
+    public static final Item PORTABLE_JUNGLE_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_jungle_chest", () -> new PortableChestItem(Portachest.asId("textures/entity/portable_jungle_chest.png"), new Item.Settings().maxCount(1)) {
+        @Override
+        public boolean canBeExtendedWith(ItemStack stack) {
+            return !stack.isEmpty() && stack.isOf(Chests.JUNGLE_CHEST.asItem());
+        }
+
+        @Override
+        public ItemStack getLarge() {
+            return PORTABLE_LARGE_JUNGLE_CHEST.getDefaultStack();
+        }
+    });
     public static final Item PORTABLE_LARGE_JUNGLE_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_large_jungle_chest", () -> new PortableLargeChestItem(Portachest.asId("textures/entity/portable_large_jungle_chest.png"), new Item.Settings().maxCount(1)));
-    public static final Item PORTABLE_ACACIA_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_acacia_chest", () -> new PortableChestItem(Portachest.asId("textures/entity/portable_acacia_chest.png"), new Item.Settings().maxCount(1)));
+    public static final Item PORTABLE_ACACIA_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_acacia_chest", () -> new PortableChestItem(Portachest.asId("textures/entity/portable_acacia_chest.png"), new Item.Settings().maxCount(1)) {
+        @Override
+        public boolean canBeExtendedWith(ItemStack stack) {
+            return !stack.isEmpty() && stack.isOf(Chests.ACACIA_CHEST.asItem());
+        }
+
+        @Override
+        public ItemStack getLarge() {
+            return PORTABLE_LARGE_ACACIA_CHEST.getDefaultStack();
+        }
+    });
     public static final Item PORTABLE_LARGE_ACACIA_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_large_acacia_chest", () -> new PortableLargeChestItem(Portachest.asId("textures/entity/portable_large_acacia_chest.png"), new Item.Settings().maxCount(1)));
-    public static final Item PORTABLE_DARK_OAK_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_dark_oak_chest", () -> new PortableChestItem(Portachest.asId("textures/entity/portable_dark_oak_chest.png"), new Item.Settings().maxCount(1)));
+    public static final Item PORTABLE_DARK_OAK_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_dark_oak_chest", () -> new PortableChestItem(Portachest.asId("textures/entity/portable_dark_oak_chest.png"), new Item.Settings().maxCount(1)) {
+        @Override
+        public boolean canBeExtendedWith(ItemStack stack) {
+            return !stack.isEmpty() && stack.isOf(Chests.DARK_OAK_CHEST.asItem());
+        }
+
+        @Override
+        public ItemStack getLarge() {
+            return PORTABLE_LARGE_DARK_OAK_CHEST.getDefaultStack();
+        }
+    });
     public static final Item PORTABLE_LARGE_DARK_OAK_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_large_dark_oak_chest", () -> new PortableLargeChestItem(Portachest.asId("textures/entity/portable_large_dark_oak_chest.png"), new Item.Settings().maxCount(1)));
-    public static final Item PORTABLE_MANGROVE_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_mangrove_chest", () -> new PortableChestItem(Portachest.asId("textures/entity/portable_mangrove_chest.png"), new Item.Settings().maxCount(1)));
+    public static final Item PORTABLE_MANGROVE_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_mangrove_chest", () -> new PortableChestItem(Portachest.asId("textures/entity/portable_mangrove_chest.png"), new Item.Settings().maxCount(1)) {
+        @Override
+        public boolean canBeExtendedWith(ItemStack stack) {
+            return !stack.isEmpty() && stack.isOf(Chests.MANGROVE_CHEST.asItem());
+        }
+
+        @Override
+        public ItemStack getLarge() {
+            return PORTABLE_LARGE_MANGROVE_CHEST.getDefaultStack();
+        }
+    });
     public static final Item PORTABLE_LARGE_MANGROVE_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_large_mangrove_chest", () -> new PortableLargeChestItem(Portachest.asId("textures/entity/portable_large_mangrove_chest.png"), new Item.Settings().maxCount(1)));
-    public static final Item PORTABLE_CHERRY_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_cherry_chest", () -> new PortableChestItem(Portachest.asId("textures/entity/portable_cherry_chest.png"), new Item.Settings().maxCount(1)));
+    public static final Item PORTABLE_CHERRY_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_cherry_chest", () -> new PortableChestItem(Portachest.asId("textures/entity/portable_cherry_chest.png"), new Item.Settings().maxCount(1)) {
+        @Override
+        public boolean canBeExtendedWith(ItemStack stack) {
+            return !stack.isEmpty() && stack.isOf(Chests.CHERRY_CHEST.asItem());
+        }
+
+        @Override
+        public ItemStack getLarge() {
+            return PORTABLE_LARGE_CHERRY_CHEST.getDefaultStack();
+        }
+    });
     public static final Item PORTABLE_LARGE_CHERRY_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_large_cherry_chest", () -> new PortableLargeChestItem(Portachest.asId("textures/entity/portable_large_cherry_chest.png"), new Item.Settings().maxCount(1)));
-    public static final Item PORTABLE_BAMBOO_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_bamboo_chest", () -> new PortableChestItem(Portachest.asId("textures/entity/portable_bamboo_chest.png"), new Item.Settings().maxCount(1)));
+    public static final Item PORTABLE_BAMBOO_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_bamboo_chest", () -> new PortableChestItem(Portachest.asId("textures/entity/portable_bamboo_chest.png"), new Item.Settings().maxCount(1)) {
+        @Override
+        public boolean canBeExtendedWith(ItemStack stack) {
+            return !stack.isEmpty() && stack.isOf(Chests.BAMBOO_CHEST.asItem());
+        }
+
+        @Override
+        public ItemStack getLarge() {
+            return PORTABLE_LARGE_BAMBOO_CHEST.getDefaultStack();
+        }
+    });
     public static final Item PORTABLE_LARGE_BAMBOO_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_large_bamboo_chest", () -> new PortableLargeChestItem(Portachest.asId("textures/entity/portable_large_bamboo_chest.png"), new Item.Settings().maxCount(1)));
-    public static final Item PORTABLE_CRIMSON_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_crimson_chest", () -> new PortableChestItem(Portachest.asId("textures/entity/portable_crimson_chest.png"), new Item.Settings().maxCount(1)));
+    public static final Item PORTABLE_CRIMSON_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_crimson_chest", () -> new PortableChestItem(Portachest.asId("textures/entity/portable_crimson_chest.png"), new Item.Settings().maxCount(1)) {
+        @Override
+        public boolean canBeExtendedWith(ItemStack stack) {
+            return !stack.isEmpty() && stack.isOf(Chests.CRIMSON_CHEST.asItem());
+        }
+
+        @Override
+        public ItemStack getLarge() {
+            return PORTABLE_LARGE_CRIMSON_CHEST.getDefaultStack();
+        }
+    });
     public static final Item PORTABLE_LARGE_CRIMSON_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_large_crimson_chest", () -> new PortableLargeChestItem(Portachest.asId("textures/entity/portable_large_crimson_chest.png"), new Item.Settings().maxCount(1)));
-    public static final Item PORTABLE_WARPED_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_warped_chest", () -> new PortableChestItem(Portachest.asId("textures/entity/portable_warped_chest.png"), new Item.Settings().maxCount(1)));
+    public static final Item PORTABLE_WARPED_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_warped_chest", () -> new PortableChestItem(Portachest.asId("textures/entity/portable_warped_chest.png"), new Item.Settings().maxCount(1)) {
+        @Override
+        public boolean canBeExtendedWith(ItemStack stack) {
+            return !stack.isEmpty() && stack.isOf(Chests.WARPED_CHEST.asItem());
+        }
+
+        @Override
+        public ItemStack getLarge() {
+            return PORTABLE_LARGE_WARPED_CHEST.getDefaultStack();
+        }
+    });
     public static final Item PORTABLE_LARGE_WARPED_CHEST = registerIf(Mods.VARIANT_VANILLA_BLOCKS.isLoaded, "portable_large_warped_chest", () -> new PortableLargeChestItem(Portachest.asId("textures/entity/portable_large_warped_chest.png"), new Item.Settings().maxCount(1)));
 
     private static Item register(String id, Item item) {
