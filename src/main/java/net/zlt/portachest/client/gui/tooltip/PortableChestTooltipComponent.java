edb25f167cf38f4f087ace55cf715c7834768194
ZLT <manult002@gmail.com>
2025-01-07 11:40:53 -0300

    add portable chest tooltip component class

---
+++ src/main/java/net/zlt/portachest/client/gui/tooltip/PortableChestTooltipComponent.java
@@ -0,0 +1,33 @@
+package net.zlt.portachest.client.gui.tooltip;
+
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.tooltip.TooltipComponent;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
+import net.zlt.portachest.client.item.PortableChestTooltipData;
+
+public class PortableChestTooltipComponent implements TooltipComponent {
+    private final DefaultedList<ItemStack> stacks;
+
+    public PortableChestTooltipComponent(PortableChestTooltipData data) {
+        stacks = data.getStacks();
+    }
+
+    @Override
+    public int getHeight() {
+        return getRows() * 20 + 6;
+    }
+
+    @Override
+    public int getWidth(TextRenderer textRenderer) {
+        return getColumns() * 18 + 2;
+    }
+
+    private int getColumns() {
+        return Math.min(9, stacks.size());
+    }
+
+    private int getRows() {
+        return (stacks.size() - 1) / 9 + 1;
+    }
+}
