b5531317800e0184275645aeaabea52a1fd83fe8
ZLT <manult002@gmail.com>
2025-01-06 21:43:08 -0300

    add abstract portable chest screen handler class

---
+++ src/main/java/net/zlt/portachest/screen/AbstractPortableChestScreenHandler.java
@@ -0,0 +1,144 @@
+package net.zlt.portachest.screen;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.screen.slot.Slot;
+import net.zlt.portachest.item.PortableChestItem;
+import org.jetbrains.annotations.Nullable;
+
+public abstract class AbstractPortableChestScreenHandler extends ScreenHandler {
+    protected final Inventory inventory;
+
+    protected AbstractPortableChestScreenHandler(@Nullable ScreenHandlerType<?> type, int syncId, Inventory inventory) {
+        super(type, syncId);
+        this.inventory = inventory;
+    }
+
+    @Override
+    public ItemStack quickMove(PlayerEntity player, int slot) {
+        ItemStack itemStack = ItemStack.EMPTY;
+        Slot slot2 = slots.get(slot);
+
+        if (slot2.hasStack()) {
+            ItemStack itemStack2 = slot2.getStack();
+            itemStack = itemStack2.copy();
+
+            if (slot < size()) {
+                if (!insertItem(itemStack2, size(), slots.size(), true)) {
+                    return ItemStack.EMPTY;
+                }
+            } else if (!insertItem(itemStack2, 0, size(), false)) {
+                return ItemStack.EMPTY;
+            }
+
+            if (itemStack2.isEmpty()) {
+                slot2.setStack(ItemStack.EMPTY);
+            } else if (slot < size()) {
+                slot2.setStack(itemStack2);
+            } else {
+                slot2.markDirty();
+            }
+        }
+
+        return itemStack;
+    }
+
+    @Override
+    public boolean canUse(PlayerEntity player) {
+        return inventory.canPlayerUse(player);
+    }
+
+    @Override
+    public void onClosed(PlayerEntity player) {
+        super.onClosed(player);
+        inventory.onClose(player);
+    }
+
+    @Override
+    protected boolean insertItem(ItemStack stack, int startIndex, int endIndex, boolean fromLast) {
+        boolean bl = false;
+        int i = startIndex;
+
+        if (fromLast) {
+            i = endIndex - 1;
+        }
+
+        if (stack.isStackable()) {
+            while (!stack.isEmpty() && (fromLast ? i >= startIndex : i < endIndex)) {
+                Slot slot = slots.get(i);
+                ItemStack itemStack = slot.getStack();
+
+                if (!itemStack.isEmpty() && ItemStack.canCombine(stack, itemStack)) {
+                    int j = itemStack.getCount() + stack.getCount();
+
+                    if (j <= stack.getMaxCount()) {
+                        stack.setCount(0);
+                        itemStack.setCount(j);
+                        slot.setStack(itemStack);
+                        bl = true;
+                    } else if (itemStack.getCount() < stack.getMaxCount()) {
+                        stack.decrement(stack.getMaxCount() - itemStack.getCount());
+                        itemStack.setCount(stack.getMaxCount());
+                        slot.setStack(itemStack);
+                        bl = true;
+                    }
+                }
+
+                if (fromLast) {
+                    --i;
+                } else {
+                    ++i;
+                }
+            }
+        }
+
+        if (!stack.isEmpty()) {
+            if (fromLast) {
+                i = endIndex - 1;
+            } else {
+                i = startIndex;
+            }
+
+            while (fromLast ? i >= startIndex : i < endIndex) {
+                Slot slot = slots.get(i);
+                ItemStack itemStack = slot.getStack();
+
+                if (itemStack.isEmpty() && slot.canInsert(stack)) {
+                    if (stack.getCount() > slot.getMaxItemCount()) {
+                        slot.setStack(stack.split(slot.getMaxItemCount()));
+                    } else {
+                        slot.setStack(stack.split(stack.getCount()));
+                    }
+
+                    slot.markDirty();
+                    bl = true;
+                    break;
+                }
+
+                if (fromLast) {
+                    --i;
+                } else {
+                    ++i;
+                }
+            }
+        }
+
+        return bl;
+    }
+
+    protected abstract int size();
+
+    protected static class PortableChestSlot extends Slot {
+        public PortableChestSlot(Inventory inventory, int index, int x, int y) {
+            super(inventory, index, x, y);
+        }
+
+        @Override
+        public boolean canInsert(ItemStack stack) {
+            return !(stack.getItem() instanceof PortableChestItem);
+        }
+    }
+}
--- src/main/java/net/zlt/portachest/screen/PortableChestScreenHandler.java
+++ src/main/java/net/zlt/portachest/screen/PortableChestScreenHandler.java
@@ -1,26 +1,19 @@
 package net.zlt.portachest.screen;
 
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.inventory.SimpleInventory;
-import net.minecraft.item.ItemStack;
-import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
-import net.zlt.portachest.item.PortableChestItem;
 
-public class PortableChestScreenHandler extends ScreenHandler {
-    private final Inventory inventory;
-
+public class PortableChestScreenHandler extends AbstractPortableChestScreenHandler {
     public PortableChestScreenHandler(int syncId, PlayerInventory playerInventory) {
         this(syncId, playerInventory, new SimpleInventory(27));
     }
 
     public PortableChestScreenHandler(int syncId, PlayerInventory playerInventory, Inventory inventory) {
-        super(AllScreenHandlerTypes.PORTABLE_CHEST, syncId);
+        super(AllScreenHandlerTypes.PORTABLE_CHEST, syncId, inventory);
 
         checkSize(inventory, 27);
-        this.inventory = inventory;
         inventory.onOpen(playerInventory.player);
 
         for (int j = 0; j < 3; ++j) {
@@ -41,125 +34,7 @@ public class PortableChestScreenHandler extends ScreenHandler {
     }
 
     @Override
-    public ItemStack quickMove(PlayerEntity player, int slot) {
-        ItemStack itemStack = ItemStack.EMPTY;
-        Slot slot2 = slots.get(slot);
-
-        if (slot2.hasStack()) {
-            ItemStack itemStack2 = slot2.getStack();
-            itemStack = itemStack2.copy();
-
-            if (slot < 27) {
-                if (!insertItem(itemStack2, 27, slots.size(), true)) {
-                    return ItemStack.EMPTY;
-                }
-            } else if (!insertItem(itemStack2, 0, 27, false)) {
-                return ItemStack.EMPTY;
-            }
-
-            if (itemStack2.isEmpty()) {
-                slot2.setStack(ItemStack.EMPTY);
-            } else if (slot < 27) {
-                slot2.setStack(itemStack2);
-            } else {
-                slot2.markDirty();
-            }
-        }
-
-        return itemStack;
-    }
-
-    @Override
-    public boolean canUse(PlayerEntity player) {
-        return inventory.canPlayerUse(player);
-    }
-
-    @Override
-    public void onClosed(PlayerEntity player) {
-        super.onClosed(player);
-        inventory.onClose(player);
-    }
-
-    @Override
-    protected boolean insertItem(ItemStack stack, int startIndex, int endIndex, boolean fromLast) {
-        boolean bl = false;
-        int i = startIndex;
-
-        if (fromLast) {
-            i = endIndex - 1;
-        }
-
-        if (stack.isStackable()) {
-            while (!stack.isEmpty() && (fromLast ? i >= startIndex : i < endIndex)) {
-                Slot slot = slots.get(i);
-                ItemStack itemStack = slot.getStack();
-
-                if (!itemStack.isEmpty() && ItemStack.canCombine(stack, itemStack)) {
-                    int j = itemStack.getCount() + stack.getCount();
-
-                    if (j <= stack.getMaxCount()) {
-                        stack.setCount(0);
-                        itemStack.setCount(j);
-                        slot.setStack(itemStack);
-                        bl = true;
-                    } else if (itemStack.getCount() < stack.getMaxCount()) {
-                        stack.decrement(stack.getMaxCount() - itemStack.getCount());
-                        itemStack.setCount(stack.getMaxCount());
-                        slot.setStack(itemStack);
-                        bl = true;
-                    }
-                }
-
-                if (fromLast) {
-                    --i;
-                } else {
-                    ++i;
-                }
-            }
-        }
-
-        if (!stack.isEmpty()) {
-            if (fromLast) {
-                i = endIndex - 1;
-            } else {
-                i = startIndex;
-            }
-
-            while (fromLast ? i >= startIndex : i < endIndex) {
-                Slot slot = slots.get(i);
-                ItemStack itemStack = slot.getStack();
-
-                if (itemStack.isEmpty() && slot.canInsert(stack)) {
-                    if (stack.getCount() > slot.getMaxItemCount()) {
-                        slot.setStack(stack.split(slot.getMaxItemCount()));
-                    } else {
-                        slot.setStack(stack.split(stack.getCount()));
-                    }
-
-                    slot.markDirty();
-                    bl = true;
-                    break;
-                }
-
-                if (fromLast) {
-                    --i;
-                } else {
-                    ++i;
-                }
-            }
-        }
-
-        return bl;
-    }
-
-    private static class PortableChestSlot extends Slot {
-        public PortableChestSlot(Inventory inventory, int index, int x, int y) {
-            super(inventory, index, x, y);
-        }
-
-        @Override
-        public boolean canInsert(ItemStack stack) {
-            return !(stack.getItem() instanceof PortableChestItem);
-        }
+    protected int size() {
+        return 27;
     }
 }
