6470b2488819b55c46f19d42a98978a322e21eb4
ZLT <manult002@gmail.com>
2025-01-06 21:17:42 -0300

    add portable large chest item class

---
+++ src/main/java/net/zlt/portachest/item/PortableLargeChestItem.java
@@ -0,0 +1,9 @@
+package net.zlt.portachest.item;
+
+import net.minecraft.util.Identifier;
+
+public class PortableLargeChestItem extends PortableChestItem {
+    public PortableLargeChestItem(Identifier entityTextureId, Settings settings) {
+        super(entityTextureId, settings);
+    }
+}
