cd48011c7494397570f674f1a36338c8ffdafd85
ZLT <manult002@gmail.com>
2025-01-06 22:06:46 -0300

    change portable large chest item screen handler factory

--- src/main/java/net/zlt/portachest/item/PortableLargeChestItem.java
+++ src/main/java/net/zlt/portachest/item/PortableLargeChestItem.java
@@ -1,9 +1,17 @@
 package net.zlt.portachest.item;
 
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import net.zlt.portachest.screen.PortableLargeChestScreenHandlerFactory;
 
 public class PortableLargeChestItem extends PortableChestItem {
     public PortableLargeChestItem(Identifier entityTextureId, Settings settings) {
         super(entityTextureId, settings);
+    }
+
+    @Override
+    public void open(PlayerEntity player, ItemStack stack) {
+        player.openHandledScreen(new PortableLargeChestScreenHandlerFactory(stack));
     }
 }
