d1513f1bd6368c0d465420191f36961ff6f22605
ZLT <manult002@gmail.com>
2025-01-07 17:44:59 -0300

    add variant vanilla blocks compat class

---
+++ src/main/java/net/zlt/portachest/compat/variantvanillablocks/VariantVanillaBlocks.java
@@ -0,0 +1,49 @@
+package net.zlt.portachest.compat.variantvanillablocks;
+
+import net.minecraft.block.Block;
+import net.xanthian.variantvanillablocks.block.Chests;
+
+public final class VariantVanillaBlocks {
+    private VariantVanillaBlocks() {
+    }
+
+    public static Block getSpruceChest() {
+        return Chests.SPRUCE_CHEST;
+    }
+
+    public static Block getBirchChest() {
+        return Chests.BIRCH_CHEST;
+    }
+
+    public static Block getJungleChest() {
+        return Chests.JUNGLE_CHEST;
+    }
+
+    public static Block getAcaciaChest() {
+        return Chests.ACACIA_CHEST;
+    }
+
+    public static Block getDarkOakChest() {
+        return Chests.DARK_OAK_CHEST;
+    }
+
+    public static Block getMangroveChest() {
+        return Chests.MANGROVE_CHEST;
+    }
+
+    public static Block getCherryChest() {
+        return Chests.CHERRY_CHEST;
+    }
+
+    public static Block getBambooChest() {
+        return Chests.BAMBOO_CHEST;
+    }
+
+    public static Block getCrimsonChest() {
+        return Chests.CRIMSON_CHEST;
+    }
+
+    public static Block getWarpedChest() {
+        return Chests.WARPED_CHEST;
+    }
+}
