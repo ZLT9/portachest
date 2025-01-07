19e58c948e47eff78fa66ef3826176e3b63651c3
ZLT <manult002@gmail.com>
2025-01-07 12:52:04 -0300

    add portable chest tooltip component draw methods

--- src/main/java/net/zlt/portachest/client/gui/tooltip/PortableChestTooltipComponent.java
+++ src/main/java/net/zlt/portachest/client/gui/tooltip/PortableChestTooltipComponent.java
@@ -3,6 +3,7 @@ package net.zlt.portachest.client.gui.tooltip;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.tooltip.TooltipComponent;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
@@ -30,11 +31,80 @@ public class PortableChestTooltipComponent implements TooltipComponent {
         return getColumns() * 18 + 2;
     }
 
+    @Override
+    public void drawItems(TextRenderer textRenderer, int x, int y, DrawContext context) {
+        int columns = getColumns();
+        int rows = getRows();
+        int k = 0;
+
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < columns; ++j) {
+                drawSlot(x + j * 18 + 1, y + i * 18 + 1, k++, context, textRenderer);
+            }
+        }
+
+        drawOutline(x, y, columns, rows, context);
+    }
+
     private int getColumns() {
         return Math.min(9, stacks.size());
     }
 
     private int getRows() {
         return (stacks.size() - 1) / 9 + 1;
+    }
+
+    private void draw(DrawContext context, int x, int y, Sprite sprite) {
+        context.drawTexture(TEXTURE, x, y, 0, sprite.u, sprite.v, sprite.width, sprite.height, 64, 64);
+    }
+
+    private void drawSlot(int x, int y, int index, DrawContext context, TextRenderer textRenderer) {
+        if (index >= stacks.size()) {
+            draw(context, x, y, Sprite.SLOT);
+            return;
+        }
+
+        ItemStack stack = stacks.get(index);
+        draw(context, x, y, Sprite.SLOT);
+        context.drawItem(stack, x + 1, y + 1, index);
+        context.drawItemInSlot(textRenderer, stack, x + 1, y + 1);
+    }
+
+    private void drawOutline(int x, int y, int columns, int rows, DrawContext context) {
+        draw(context, x, y, Sprite.BORDER_CORNER);
+        draw(context, x + columns * 18 + 1, y, Sprite.BORDER_CORNER);
+
+        for (int i = 0; i < columns; ++i) {
+            draw(context, x + 1 + i * 18, y, Sprite.BORDER_HORIZONTAL);
+            draw(context, x + 1 + i * 18, y + rows * 18 + 1, Sprite.BORDER_HORIZONTAL);
+        }
+
+        for (int i = 0; i < rows; ++i) {
+            draw(context, x, y + i * 18 + 1, Sprite.BORDER_VERTICAL);
+            draw(context, x + columns * 18 + 1, y + i * 18 + 1, Sprite.BORDER_VERTICAL);
+        }
+
+        draw(context, x, y + rows * 18 + 1, Sprite.BORDER_CORNER);
+        draw(context, x + columns * 18 + 1, y + rows * 18 + 1, Sprite.BORDER_CORNER);
+    }
+
+    @Environment(EnvType.CLIENT)
+    private enum Sprite {
+        SLOT(0, 0, 18, 18),
+        BORDER_VERTICAL(0, 18, 1, 18),
+        BORDER_HORIZONTAL(0, 18, 18, 1),
+        BORDER_CORNER(0, 18, 1, 1);
+
+        public final int u;
+        public final int v;
+        public final int width;
+        public final int height;
+
+        Sprite(int u, int v, int width, int height) {
+            this.u = u;
+            this.v = v;
+            this.width = width;
+            this.height = height;
+        }
     }
 }
