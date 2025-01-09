76d6571a4768de3271fa2e0f4a9b9576c4043dce
ZLT <manult002@gmail.com>
2025-01-09 02:04:45 -0300

    add mod menu entrypoint

---
+++ src/main/java/net/zlt/portachest/compat/modmenu/PortachestModMenuApiImpl.java
@@ -0,0 +1,9 @@
+package net.zlt.portachest.compat.modmenu;
+
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class PortachestModMenuApiImpl implements ModMenuApi {
+}
