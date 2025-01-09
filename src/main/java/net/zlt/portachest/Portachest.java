b49491f29c5abe16d35f6418aca2dbfed7b99d92
ZLT <manult002@gmail.com>
2025-01-09 16:08:19 -0300

    use config from packet

--- src/main/java/net/zlt/portachest/Portachest.java
+++ src/main/java/net/zlt/portachest/Portachest.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 import net.zlt.portachest.compat.Mods;
 import net.zlt.portachest.compat.trinkets.Trinkets;
@@ -11,6 +12,7 @@ import net.zlt.portachest.item.AllItemGroups;
 import net.zlt.portachest.item.AllItems;
 import net.zlt.portachest.item.PortableChestItem;
 import net.zlt.portachest.networking.AllNetworkingConstants;
+import net.zlt.portachest.option.PortableChestSlotPriorityLevel;
 import net.zlt.portachest.recipe.AllRecipeSerializers;
 import net.zlt.portachest.screen.AllScreenHandlerTypes;
 import org.slf4j.Logger;
@@ -32,25 +34,72 @@ public class Portachest implements ModInitializer {
         if (Mods.TRINKETS.isLoaded) {
             Trinkets.init();
         } else {
-            ServerPlayNetworking.registerGlobalReceiver(AllNetworkingConstants.OPEN_PORTABLE_CHEST_PACKET_ID, (server, player, handler, buf, responseSender) -> server.execute(() -> {
-                ItemStack chestStack = player.getEquippedStack(EquipmentSlot.CHEST);
-                if (chestStack.getItem() instanceof PortableChestItem portableChest) {
-                    portableChest.open(player, chestStack);
-                    return;
-                }
+            ServerPlayNetworking.registerGlobalReceiver(AllNetworkingConstants.OPEN_PORTABLE_CHEST_PACKET_ID, (server, player, handler, buf, responseSender) -> {
+                int level1 = buf.readInt();
+                int level2 = buf.readInt();
+                int level3 = buf.readInt();
+                int level4 = buf.readInt();
 
-                ItemStack mainHandStack = player.getMainHandStack();
-                if (mainHandStack.getItem() instanceof PortableChestItem portableChest) {
-                    portableChest.open(player, mainHandStack);
-                    return;
-                }
-
-                ItemStack offHandStack = player.getOffHandStack();
-                if (offHandStack.getItem() instanceof PortableChestItem portableChest) {
-                    portableChest.open(player, offHandStack);
-                }
-            }));
+                server.execute(() -> {
+                    if (openPortableChestIn(level1, player)) {
+                        return;
+                    }
+                    if (openPortableChestIn(level2, player)) {
+                        return;
+                    }
+                    if (openPortableChestIn(level3, player)) {
+                        return;
+                    }
+                    openPortableChestIn(level4, player);
+                });
+            });
         }
+    }
+
+    public static boolean openPortableChestInChestSlot(ServerPlayerEntity player) {
+        ItemStack chestStack = player.getEquippedStack(EquipmentSlot.CHEST);
+        if (chestStack.getItem() instanceof PortableChestItem portableChest) {
+            portableChest.open(player, chestStack);
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean openPortableChestInMainHand(ServerPlayerEntity player) {
+        ItemStack mainHandStack = player.getMainHandStack();
+        if (mainHandStack.getItem() instanceof PortableChestItem portableChest) {
+            portableChest.open(player, mainHandStack);
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean openPortableChestInOffHand(ServerPlayerEntity player) {
+        ItemStack offHandStack = player.getOffHandStack();
+        if (offHandStack.getItem() instanceof PortableChestItem portableChest) {
+            portableChest.open(player, offHandStack);
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean openPortableChestIn(int level, ServerPlayerEntity player) {
+        if (level == PortableChestSlotPriorityLevel.VANILLA_CHEST.ordinal()) {
+            return openPortableChestInChestSlot(player);
+        }
+        if (level == PortableChestSlotPriorityLevel.TRINKETS_CHEST_BACK.ordinal()) {
+            return false;
+        }
+        if (level == PortableChestSlotPriorityLevel.VANILLA_MAIN_HAND.ordinal()) {
+            return openPortableChestInMainHand(player);
+        }
+        if (level == PortableChestSlotPriorityLevel.VANILLA_OFF_HAND.ordinal()) {
+            return openPortableChestInOffHand(player);
+        }
+        return false;
     }
 
     public static Identifier asId(String path) {
--- src/main/java/net/zlt/portachest/compat/trinkets/Trinkets.java
+++ src/main/java/net/zlt/portachest/compat/trinkets/Trinkets.java
@@ -4,12 +4,14 @@ import dev.emi.trinkets.api.SlotReference;
 import dev.emi.trinkets.api.TrinketComponent;
 import dev.emi.trinkets.api.TrinketsApi;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Pair;
+import net.zlt.portachest.Portachest;
 import net.zlt.portachest.item.PortableChestItem;
 import net.zlt.portachest.networking.AllNetworkingConstants;
+import net.zlt.portachest.option.PortableChestSlotPriorityLevel;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
@@ -19,35 +21,56 @@ public final class Trinkets {
     }
 
     public static void init() {
-        ServerPlayNetworking.registerGlobalReceiver(AllNetworkingConstants.OPEN_PORTABLE_CHEST_PACKET_ID, (server, player, handler, buf, responseSender) -> server.execute(() -> {
-            ItemStack chestStack = player.getEquippedStack(EquipmentSlot.CHEST);
-            if (chestStack.getItem() instanceof PortableChestItem portableChest) {
-                portableChest.open(player, chestStack);
-                return;
-            }
+        ServerPlayNetworking.registerGlobalReceiver(AllNetworkingConstants.OPEN_PORTABLE_CHEST_PACKET_ID, (server, player, handler, buf, responseSender) -> {
+            int level1 = buf.readInt();
+            int level2 = buf.readInt();
+            int level3 = buf.readInt();
+            int level4 = buf.readInt();
 
-            Optional<TrinketComponent> trinketComponent = TrinketsApi.getTrinketComponent(player);
-            if (trinketComponent.isPresent()) {
-                for (Pair<SlotReference, ItemStack> equipped : trinketComponent.get().getAllEquipped()) {
-                    ItemStack stack = equipped.getRight();
-                    if (stack.getItem() instanceof PortableChestItem portableChest) {
-                        portableChest.open(player, stack);
-                        return;
-                    }
+            server.execute(() -> {
+                if (openPortableChestIn(level1, player)) {
+                    return;
+                }
+                if (openPortableChestIn(level2, player)) {
+                    return;
+                }
+                if (openPortableChestIn(level3, player)) {
+                    return;
+                }
+                openPortableChestIn(level4, player);
+            });
+        });
+    }
+
+    public static boolean openPortableChestInBackTrinketSlot(ServerPlayerEntity player) {
+        Optional<TrinketComponent> trinketComponent = TrinketsApi.getTrinketComponent(player);
+        if (trinketComponent.isPresent()) {
+            for (Pair<SlotReference, ItemStack> equipped : trinketComponent.get().getAllEquipped()) {
+                ItemStack stack = equipped.getRight();
+                if (stack.getItem() instanceof PortableChestItem portableChest) {
+                    portableChest.open(player, stack);
+                    return true;
                 }
             }
+        }
 
-            ItemStack mainHandStack = player.getMainHandStack();
-            if (mainHandStack.getItem() instanceof PortableChestItem portableChest) {
-                portableChest.open(player, mainHandStack);
-                return;
-            }
+        return false;
+    }
 
-            ItemStack offHandStack = player.getOffHandStack();
-            if (offHandStack.getItem() instanceof PortableChestItem portableChest) {
-                portableChest.open(player, offHandStack);
-            }
-        }));
+    public static boolean openPortableChestIn(int level, ServerPlayerEntity player) {
+        if (level == PortableChestSlotPriorityLevel.VANILLA_CHEST.ordinal()) {
+            return Portachest.openPortableChestInChestSlot(player);
+        }
+        if (level == PortableChestSlotPriorityLevel.TRINKETS_CHEST_BACK.ordinal()) {
+            return openPortableChestInBackTrinketSlot(player);
+        }
+        if (level == PortableChestSlotPriorityLevel.VANILLA_MAIN_HAND.ordinal()) {
+            return Portachest.openPortableChestInMainHand(player);
+        }
+        if (level == PortableChestSlotPriorityLevel.VANILLA_OFF_HAND.ordinal()) {
+            return Portachest.openPortableChestInOffHand(player);
+        }
+        return false;
     }
 
     @Nullable
