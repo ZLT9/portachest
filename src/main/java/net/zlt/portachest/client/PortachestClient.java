51c2822fde5393303b599769e2d9e08368206a6b
ZLT <manult002@gmail.com>
2025-01-09 16:08:32 -0300

    init config

--- src/main/java/net/zlt/portachest/client/PortachestClient.java
+++ src/main/java/net/zlt/portachest/client/PortachestClient.java
@@ -16,6 +16,7 @@ import net.zlt.portachest.client.gui.screen.ingame.PortableLargeChestScreen;
 import net.zlt.portachest.client.gui.tooltip.PortableChestTooltipComponent;
 import net.zlt.portachest.client.item.PortableChestTooltipData;
 import net.zlt.portachest.client.option.AllKeyBindings;
+import net.zlt.portachest.client.option.Config;
 import net.zlt.portachest.compat.Mods;
 import net.zlt.portachest.screen.AllScreenHandlerTypes;
 
@@ -34,5 +35,6 @@ public class PortachestClient implements ClientModInitializer {
         TooltipComponentCallback.EVENT.register(data -> data instanceof PortableChestTooltipData portableChestTooltipData ? new PortableChestTooltipComponent(portableChestTooltipData) : null);
 
         AllKeyBindings.init();
+        Config.init();
     }
 }
