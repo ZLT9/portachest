da0690e215c9c66ebc1f759b4ef956dee6c98763
ZLT <manult002@gmail.com>
2025-01-09 16:09:08 -0300

    add mod menu config screen

--- src/main/java/net/zlt/portachest/compat/modmenu/PortachestModMenuApiImpl.java
+++ src/main/java/net/zlt/portachest/compat/modmenu/PortachestModMenuApiImpl.java
@@ -1,9 +1,15 @@
 package net.zlt.portachest.compat.modmenu;
 
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.zlt.portachest.client.gui.screen.option.ConfigScreen;
 
 @Environment(EnvType.CLIENT)
 public class PortachestModMenuApiImpl implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return ConfigScreen::new;
+    }
 }
